f6266a2407578080eda801b610113735e748224d
Raisa-NN <rnnkweteyim@miners.utep.edu>
2020-02-24 14:51:39 -0700

    Almost finished Exhaustive Checks method

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -16,23 +16,41 @@ public class SnakeGame {
         //to test, do I need to hardcode the board with the snake?
     }//complete exhaustive search by Saturday evening
 
-    public int neighbours(int row, int col) {
-        return 1;
+    public int neighbours(int row, int col) { //Helper method to count the neighbours
+        int neighbour = 0;
+        for (row = 0; row < game.length-1; row++) {
+            for (col = 0; col < game[row].length-1; col++) {
+                if (game[row][col+1] == true && game[row+1][col] == true && row <) {
+                    neighbour ++;
+                }
+            }
+        }
+        if (game[row + 1] == true) {
+            return 1;
+        }
     }
 
     public int[] findTailExhaustive() {
         exhaustiveChecks = 0;
         int length = 0;
-        for (int i = 0; i < game.length; i++) { //game.length-1 so that right neighbour won't be out of bounds
-            for (int j = 0; j < game[i].length; j++) {
+        int[] result = new int[3];
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < game.length; i++) {
+            for (j = 0; j < game[i].length; j++) {
+                exhaustiveChecks ++;
                 if (game[i][j] == true) {
                     length++;
-                    exhaustiveChecks ++;
-                    int sumNeighbours = neighbours(i, j); //Calling neighbours method to see if cell is a head/tail or none
-                    if (sumNeighbours == 1) {}
+                    int sumNeighbours = neighbours(i, j); //Calling neighbours method to count number of neighbours and see if cell is a head/tail or none
+                    if (sumNeighbours == 1) { // Checking to see if cell is a head/tail
+                        if (!game[headPosition[0]][headPosition[1]]) { //If the cell is tail
+                            exhaustiveChecks --;
+                        }
+                    }
                 }
             }
         }
+        return result = new int[]{i, j, length};
     }
 
 }
