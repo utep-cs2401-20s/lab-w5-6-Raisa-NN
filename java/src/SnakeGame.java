5af64557d9e3a34302fd7240b0fb18c9df3baeb9
Raisa-NN <rnnkweteyim@miners.utep.edu>
2020-02-26 14:50:19 -0700

    Finished Exhaustive Checks method

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -9,48 +9,98 @@ public class SnakeGame {
     }
 
     public SnakeGame(boolean[][] game, int x, int y) {
-        this.game = game;
+        this.game = game;//use nested for loop?
         this.headPosition = new int[2];
         headPosition[0] = x;
         headPosition[1] = y;
-        //to test, do I need to hardcode the board with the snake?
-    }//complete exhaustive search by Saturday evening
+    }
 
-    public int neighbours(int row, int col) { //Helper method to count the neighbours
+
+    private void resetCounters() {
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getExhaustiveChecks() {//in test cases, make it public; it has to be static because method is static
+        return exhaustiveChecks;
+    }
+
+    private static int getRecursiveChecks() {
+        return recursiveChecks;
+    }
+
+    public int neighbours(int row, int col) { //Helper method to count the 4 possible neighbours
         int neighbour = 0;
-        for (row = 0; row < game.length-1; row++) {
-            for (col = 0; col < game[row].length-1; col++) {
-                if (game[row][col+1] == true && game[row+1][col] == true && row <) {
-                    neighbour ++;
-                }
-            }
+        if (game[row][col + 1] && col + 1 < game.length) { //If right neighbour is a snake and is in the board
+            neighbour++;
         }
-        if (game[row + 1] == true) {
-            return 1;
+        if (game[row + 1][col] && row + 1 < game.length) { //If bottom neighbour is a snake and is in the board
+            neighbour++;
         }
+        if (game[row - 1][col] && row - 1 >= 0) { //If top neighbour is a snake and is in the board
+            neighbour++;
+        }
+        if (game[row][col - 1] && col - 1 >= 0) { //If left neighbour is a snake and is in the board
+            neighbour++;
+        }
+        return neighbour;
     }
 
     public int[] findTailExhaustive() {
-        exhaustiveChecks = 0;
+        resetCounters();
         int length = 0;
         int[] result = new int[3];
-        int i = 0;
-        int j = 0;
-        for (i = 0; i < game.length; i++) {
-            for (j = 0; j < game[i].length; j++) {
-                exhaustiveChecks ++;
-                if (game[i][j] == true) {
+        int tailX = 0; int tailY = 0;
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                int sumNeighbours = neighbours(i, j); //Calling neighbours method to count number of neighbours and see if cell is a head/tail or none
+                if (sumNeighbours == 1) { // Checking to see if cell is a head/tail
+                    if (!game[headPosition[0]][headPosition[1]]) { //If the cell is tail. That is, if tail found
+                        tailX = i;
+                        tailY = j;
+                    } else {
+                        exhaustiveChecks ++;
+                    }
+                if (game[i][j]) { //If part of snake
                     length++;
-                    int sumNeighbours = neighbours(i, j); //Calling neighbours method to count number of neighbours and see if cell is a head/tail or none
-                    if (sumNeighbours == 1) { // Checking to see if cell is a head/tail
-                        if (!game[headPosition[0]][headPosition[1]]) { //If the cell is tail
-                            exhaustiveChecks --;
-                        }
                     }
                 }
             }
         }
-        return result = new int[]{i, j, length};
+        return result = new int[]{tailX, tailY, length};
     }
 
+    public int[] findTailRecursive() {
+        resetCounters();
+        int[] result = findTailRecursive(headPosition, headPosition);
+        return result;
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition) {
+        resetCounters();
+        recursiveChecks ++;
+        int length = 1; //Counts head
+        int[] result = new int[3]; //must always go to a cell thathas a snake, count leg=ngth, only increment for calls that has snake
+        int tailX = 0; int tailY = 0;
+        int sumNeighbours = neighbours(currentPosition[0], currentPosition[1]); //Get the neighbours of head
+        if (sumNeighbours == 1 && !game[headPosition[0]][headPosition[1]]) { //If tail (base case)
+            return  result = new int[]{headPosition[0], headPosition[1], length};
+        }
+        if (sumNeighbours >= 1) { // If along snake
+            if (game[currentPosition[0]][currentPosition[1] + 1] && currentPosition[1] + 1 < game.length); ////If right neighbour is part of snake and is in the board
+            int[] temp = currentPosition;
+            currentPosition[0]++;
+            currentPosition[1]++;
+            previousPosition = temp;
+            length++;
+            findTailRecursive();
+        }
+        if (sumNeighbours == 1 && currentPosition != previousPosition) { //If at tail
+            tailX = currentPosition[0];
+            tailY = currentPosition[1];
+        } else {
+            findTailRecursive();
+        }
+        return result = new int[]{tailX, tailY, length};
+    }
 }
---
+++ java/src/SnakeGameTester.java
@@ -0,0 +1,6 @@
+import org.junit.jupiter.api.Test;
+
+public class SnakeGameTester {
+    @Test
+    public void
+}//make sure that you're nit overcounting cells, you're counting right number of cells. whether tail found correctly and length correctly. Each each case, test one thing
